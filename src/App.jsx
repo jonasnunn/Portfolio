b74f9383443492ebc4a5cfb6507ef1bb50eaa7e0
jonasnunn <jonasnunn@gmail.com>
2023-06-05 12:17:20 -0600

    populated project component with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import Home from "./Home.jsx";
 import Project from "./Components/Project.jsx";
 
-
 function App() {
-    return (
-        <>
-        <Home></Home>
-        <Project></Project>
-        </>
-    );
+  return (
+    <>
+      <Home></Home>
+      <Project
+        name="Old Faithful Fountains"
+        url="https://github.com/jonasnunn/old_faithful"
+        description="I built this website for a small company to improve their online presence, and give consumers knowledge of the company."
+      />
+    </>
+  );
 }
 
 export default App;
--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,25 +1,31 @@
 import styles from "../styles/home.css";
-let projectName = "Old Faithful Fountains";
-const projectUrl = "https://github.com/jonasnunn/old_faithful";
-const projectDescription =
-  "I built this website for a small company to improve their online presence, and give consumers knowledge of the company.";
+import PropTypes from 'prop-types';
+// let projectName = "Old Faithful Fountains";
+// const projectUrl = "https://github.com/jonasnunn/old_faithful";
+// const projectDescription = "I built this website for a small company to improve their online presence, and give consumers knowledge of the company.";
 
-function Project() {
+const Project = (props) => {
+  const { name, url, description} = props;
+
   return (
-    <>
       <div className={styles.main}>
-        <h2>{projectName}</h2>
-        <p>{projectDescription}</p>
+        <h2>{name}</h2>
+        <p>{description}</p>
         <p>
           Repository:
-          <a href={projectUrl} target="_blank" rel="noreferrer">
+          <a href={url} target="_blank" rel="noreferrer">
             {" "}
-            {projectUrl}
+            {url}
           </a>
         </p>
       </div>
-    </>
   );
 }
+
+Project.propTypes = {
+  name: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 
 export default Project;
